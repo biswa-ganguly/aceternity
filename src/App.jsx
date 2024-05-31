41034aeaa7ed61a04f13fabb53571f01f125b289
biswa-ganguly <biswaganguly16@gmail.com>
2024-05-31 16:04:35 +0530

    aceternity in vite with javascript

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ThreeDCardDemo } from './Test'
+
+function App() {
+  return (
+    <div className='w-screen h-screen bg-zinc-800'>
+      <ThreeDCardDemo/>
+    </div>
+  )
+}
+
+export default App
